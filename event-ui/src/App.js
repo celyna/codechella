9304fad7fc340ed9d98271122fe33112cb1c7bde
andrewchen349 <andrewchen349@gmail.com>
2020-11-20 20:43:13 -0500

    Added basic naviagtion dom

--- event-ui/src/App.js
+++ event-ui/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Import Pages
+import Home from './pages/Home';
+
+//Other Imports
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      data: null,
+    }
+  }
+
+  // componentDidMount(){
+  //   axios.get(``)
+  //     .then(res => {
+  //       this.setState({data: res.json()})
+  //     })
+  // }
+
+  render(){
+    return(
+      <Router>
+        <Container>
+          <Navbar bg="transparent" expand="lg">
+            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+                <Nav className="ml-auto">
+                    <h5> <Link className="nav-link display-5 font-weight-semibold subheading" to="/">Home</Link></h5>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+            <Route path="/" exact render={() => <Home />} />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ event-ui/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>Home Page</div>
+        );
+    }
+}
+export default Home;
