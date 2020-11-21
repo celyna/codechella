c445ed7bc85f3e7d8ec8295c1298391df2f25f0f
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2020-11-21 15:42:05 +0000

    Flask backend

--- event-backend/api.py
+++ event-backend/api.py
@@ -1,5 +1,4 @@
 import time
-import tweepy
 import key
 import os
 import dataset
@@ -13,75 +12,18 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-
-
-
-
-def read_emolex(filepath=None):
-    '''
-    Takes a file path to the emolex lexicon file.
-    Returns a dictionary of emolex sentiment values.
-    '''
-    if filepath==None: # Try to find the emolex file
-        filepath = os.path.join('emolex.txt')
-        if os.path.isfile(filepath):
-            pass
-        elif os.path.isfile('emolex.txt'):
-            filepath = 'emolex.txt'
-        else:
-            raise FileNotFoundError('No EmoLex file found')
-    emolex = defaultdict(dict) # Like Counter(), defaultdict eases dictionary creation
-    with open(filepath, 'r') as f:
-    # emolex file format is: word emotion value
-        for line in f:
-            word, emotion, value = line.strip().split()
-            emolex[word][emotion] = int(value)
-    return emolex
-
-
-def tokenize_text(text, stopwords=None):
-    '''
-    Takes a string.
-    Returns a list of tokenized sentences.
-    '''
-    tokenized_text = []
-    for sent in sent_tokenize(text):
-        tokens = word_tokenize(sent)
-        if stopwords != None:
-            tokens = [token for token in tokens if token not in stopwords]
-        tokenized_text.append(tokens)
-    return tokenized_text
-
-
-def sentiment_score(token_list, lexicon=None):
-    output = {
-        'anger': 0.0,
-        'anticipation': 0.0,
-        'disgust': 0.0,
-        'fear': 0.0,
-        'joy': 0.0,
-        'negative': 0.0,
-        'positive': 0.0,
-        'sadness': 0.0,
-        'surprise': 0.0,
-        'trust': 0.0
-    }
-    emolex = read_emolex(emolex_file)
-    for token in token_list[0]:
-        if token.lower() in emolex:
-            for emo in emolex[token.lower()]:
-                if emolex[token.lower()].get(emo) == 1:
-                    output.update({emo: emolex[token.lower()].get(emo) / len(token_list)})
-    return output
-
+db = dataset.connect('sqlite:///tweet.db')
+table = db.load_table('population')
 
 
 @app.route('/')
 def index():
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(track=['twitter'], is_async=True)
-    return jsonify(myStream)
+    return jsonify(time.time())
+
+
+@app.route('/search/<eventname>')
+def search(eventname):
+    pass
 
 
 @app.route('/time')
@@ -92,18 +34,5 @@ def get_current_time():
 @app.route('/result', methods=['POST'])
 def getEvent():
     # data = request.json
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    # myStream.filter(track=[data.get('event')])
-    myStream.filter(track=['python'])
-    app.run(debug=True)
 
     return "OK"
-
-# if __name__ == '__main__':
-#     print("starting")
-#     myStreamListener = MyStreamListener()
-#     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-#     myStream.filter(track=['machine learning'])
-#     app.run(debug=True)
-    # print(read_emolex(emolex_file))
