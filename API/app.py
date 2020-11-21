c836e8f3a4259314e358bc93801f27f9bb3a052d
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2020-11-21 01:07:27 +0000

    Flask backend

---
+++ API/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flASK_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route('/')
+def index():
+    # return jsonify('')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
