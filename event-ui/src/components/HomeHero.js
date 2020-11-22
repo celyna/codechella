86a043336e43d6d63b011bea373e76f0f21da41f
andrewchen349 <andrewchen349@gmail.com>
2020-11-22 04:25:42 -0500

    fix color

--- event-ui/src/components/HomeHero.js
+++ event-ui/src/components/HomeHero.js
@@ -12,7 +12,7 @@ class HomeHero extends React.Component{
             <Container fluid={true}>
                 <Row className="justify-content-center mt-5">
                     <Col className="text-left">
-                        <h2 className="display-4 font-weight-bolder justift-content-center"><span style={{color:'#A3CEF1'}}>SafeTweet</span></h2>
+                        <h2 className="display-4 font-weight-bolder justift-content-center"><span style={{color:'#274C77'}}>SafeTweet</span></h2>
                         <h4 className="display-6 font-weight-light">Explore or learn more about your next events from others</h4> 
                         <h6 className="display-6 font-weight-light">Make sure events are safe and fun!</h6> 
                     </Col>
